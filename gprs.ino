276cf16a44974afe20e73f19ba2c575cb5a816b8
Алмаз <120911287+Ga1maz@users.noreply.github.com>
2024-10-28 10:10:20 +0500

    Update and rename block1.ino to gprs.ino

---
+++ gprs.ino
@@ -0,0 +1,53 @@
+#include <GPRS_Shield_Arduino.h>
+
+// номер на который будем звонить
+#define PHONE_NUMBER  "+79177537900"
+#define TEXT "Train!"
+
+// создаём объект класса GPRS и передаём в него объект Serial1 
+GPRS gprs(Serial1);
+
+// флаг для предотвращения повторного выполнения
+bool actionExecuted = false;
+
+void setup() {
+  // открываем последовательный порт для мониторинга действий в программе
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  Serial.print("Serial init OK\r\n");
+
+  // инициализация GPRS модуля
+  Serial1.begin(9600);
+  gprs.powerOn();
+  while (!gprs.init()) {
+    delay(1000);
+    Serial.print("GPRS Init error\r\n");
+  }
+  Serial.println("GPRS init success");
+
+  // настройка пина 7 как входа с подтягивающим резистором
+  pinMode(7, INPUT_PULLUP);
+  
+  // настройка пина 8 как выхода
+  pinMode(8, OUTPUT);
+  digitalWrite(8, LOW);  // по умолчанию установим LOW на пине 8
+}
+
+void loop() {
+  // Проверяем, если на 7 пине GND и действие ещё не выполнено
+  if (digitalRead(7) == LOW && !actionExecuted) {
+    // Устанавливаем высокий уровень (5V) на пине 8 навсегда
+    digitalWrite(8, HIGH);
+    Serial.print("Start to sms");
+    gprs.sendSMS(PHONE_NUMBER, TEXT);
+    delay(5000);
+
+    Serial.print("Start to call ");
+    Serial.print(PHONE_NUMBER);
+    gprs.callUp(PHONE_NUMBER);
+
+    // Устанавливаем флаг, чтобы не выполнять действие повторно
+    actionExecuted = true;
+  }
+}
