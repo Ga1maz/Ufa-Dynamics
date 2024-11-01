aad5a35a8fbc15cc10004ac9f51001b2eb4e2e6e
CapitanOfUfaDynamics <rageofenglishopening@gmail.com>
2024-11-01 15:57:37 +0500

    Update gprs.ino

--- gprs.ino
+++ gprs.ino
@@ -4,6 +4,16 @@
 #define PHONE_NUMBER  "+79177537900"
 #define TEXT "Train!"
 
+// Библиотека для работы с дальномерами
+#include "TFLidar.h"
+ 
+// Библиотека для работы программного Serial
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11);
+// Serial-порт к которому подключён дальномер
+#define LIDAR_SERIAL    mySerial
+TFLidar lidar;
+int dist;
 // создаём объект класса GPRS и передаём в него объект Serial1 
 GPRS gprs(Serial1);
 
@@ -32,11 +42,22 @@ void setup() {
   // настройка пина 8 как выхода
   pinMode(8, OUTPUT);
   digitalWrite(8, LOW);  // по умолчанию установим LOW на пине 8
+    // Открываем последовательный порт для мониторинга действий в программе
+  // и передаём скорость 9600 бод
+  Serial.begin(9600);
+  // Ждём, пока не откроется монитор последовательного порта
+  // для того, чтобы отследить все события в программе
+  while (!Serial);
+ 
+  Serial.print("Serial init OK\r\n");
+  // Открываем Serial-соединение с дальномером на скорости 115200
+  LIDAR_SERIAL.begin(115200);
+  lidar.begin(&LIDAR_SERIAL);  
 }
 
 void loop() {
   // Проверяем, если на 7 пине GND и действие ещё не выполнено
-  if (digitalRead(7) == LOW && !actionExecuted) {
+  if (lidar.getData(dist) == LOW && !actionExecuted) {
     // Устанавливаем высокий уровень (5V) на пине 8 навсегда
     digitalWrite(8, HIGH);
     Serial.print("Start to sms");
