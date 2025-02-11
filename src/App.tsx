e4b30b0d4ba45482250bfc1f47576b8876c7fbf9
Almaz Ganiev <aganiev806@gmail.com>
2025-02-11 22:45:39 +0500

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body id="_" cz-shortcut-listen="true">
+      <div id="_1_">
+        <div id="Group_7">
+          <div id="Group_4">
+            <div id="Rectangle_34" class="pointer-events-none">
+            </div>
+          </div>
+          <div id="Group_1">
+            <div id="Rectangle_31" class="pointer-events-none">
+            </div>
+            <span id="_57_7" class="pointer-events-none"><span id="__0">UFA-Dynamics</span></span>
+          </div>
+          <div id="Group_2">
+            <div id="Rectangle_32" class="pointer-events-none">
+            </div>
+            <span id="_57_10" class="pointer-events-none"><span id="__1">карта<br id="__2"></br>YANDEX MAPS<br id="__3"></br>Серая
+                тема <br id="__4"></br> ночь</span></span>
+          </div>
+          <div id="Group_3">
+            <div id="Rectangle_33" class="pointer-events-none">
+            </div>
+          </div>
+          <span id="_57_13" class="pointer-events-none"><span id="__5">Аккумулятор</span></span>
+          <svg width="201" height="122" viewBox="0 0 201 122" fill="none" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" id="_2025_01_10_18_20_52_1"
+            class="pointer-events-none">
+            <defs id="__6">
+              <image
+                href="https://leonie-c8ekn7e2ux.figweb.site/cdn-cgi/imagedelivery/s-dfVpmPR-aKHmwFNwAgnQ/leonie-c8ekn7e2ux.figweb.site-40f88f37d722aa048f8fc232b9c79d87763bd23e/public"
+                id="def_0">
+              </image>
+              <pattern width="201" height="122" patternUnits="userSpaceOnUse" id="def_1">
+                <use href="#def_0"
+                  transform="matrix(0.5689375141368019 0 0 0.8810017054728707 -413.98819608754434 -96.85865856653368)"
+                  id="__7">
+                </use>
+              </pattern>
+            </defs>
+            <path d="M0 0H201V122H0V0Z" id="_2025_01_10_18_20_52_1_0"></path>
+          </svg>
+          <span id="_57_15" class="pointer-events-none"><span id="__8"><span id="__9">100</span><span
+                id="__10">%</span></span></span>
+          <div id="Rectangle_35" class="pointer-events-none">
+          </div>
+          <span id="_57_17" class="pointer-events-none"><span id="__11">Battery replacement</span></span>
+          <div id="Group_5">
+            <div id="Rectangle_36" class="pointer-events-none">
+            </div>
+            <span id="_57_20" class="pointer-events-none"><span id="__12">required</span></span>
+          </div>
+          <span id="_57_21" class="pointer-events-none"><span id="__13"> [54.753510, 32.058214]</span></span>
+          <svg preserveAspectRatio="none" id="Arrow_1" class="pointer-events-none" width="703" height="16"
+            viewBox="0 0 703 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M702.707 8.70711C703.098 8.31658 703.098 7.68342 702.707 7.29289L696.343 0.928932C695.953 0.538408 695.319 0.538408 694.929 0.928932C694.538 1.31946 694.538 1.95262 694.929 2.34315L700.586 8L694.929 13.6569C694.538 14.0474 694.538 14.6805 694.929 15.0711C695.319 15.4616 695.953 15.4616 696.343 15.0711L702.707 8.70711ZM0 9H702V7H0V9Z"
+              fill="black"></path>
+          </svg>
+          <span id="_57_23" class="pointer-events-none"><span id="__14">[32.058214, 32.058214]</span></span>
+          <div id="Rectangle_37" class="pointer-events-none">
+          </div>
+          <span id="_57_25" class="pointer-events-none"><span id="__15">12 km</span></span>
+          <div id="Rectangle_38" class="pointer-events-none">
+          </div>
+          <span id="_57_27" class="pointer-events-none"><span id="__16">Системные данные</span></span>
+          <div id="Rectangle_39" class="pointer-events-none">
+          </div>
+          <span id="_57_29" class="pointer-events-none"><span id="__17">Температура</span></span>
+        </div>
+      </div>
+      </body>
+    </>
+  )
+}
+
+export default App
